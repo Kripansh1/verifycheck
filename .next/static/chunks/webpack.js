679bf0170a194cf42c29c7d45365321359ea4dfa
Kripansh1 <verifykart2@gmail.com>
2025-08-31 20:35:21 +0530

    chore(vercel): switch to Next.js build, set Node 22, neutralize CRA router files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9583cfa51b12a2fc"; }
+/******/ 		__webpack_require__.h = function() { return "15c287b34aac0492"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
