1a39c1fe7a13b882e44c102eed922b40352dd0ca
Kripansh1 <verifykart2@gmail.com>
2025-09-09 10:48:45 +0530

    Remove all Google tracking codes from profile verification page
    - Removed Google Tag Manager (GTM-NHX8V7V2) script and noscript tags
    - Removed Google Analytics gtag (AW-17513233006) tracking code
    - Clean slate for fresh tracking setup from scratch
    - Page now has no tracking codes in preparation for new implementation

--- pages/b2c/profile-verification.tsx
+++ pages/b2c/profile-verification.tsx
@@ -15,42 +15,8 @@ const B2CProfileVerification = () => {
   return (
     <>
       <Head>
-        {/* Google Tag Manager for B2C */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NHX8V7V2');
-            `,
-          }}
-        />
-
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17513233006" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-17513233006');
-            `,
-          }}
-        />
       </Head>
       <div className="min-h-screen flex flex-col bg-white">
-        {/* Google Tag Manager (noscript) for B2C */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NHX8V7V2"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
 
         <Header />
         <main className="flex-grow">
