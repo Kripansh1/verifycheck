4b1479a845f3cde0cc0d4a34982d6d1fe246a3b1
Kripansh1 <verifykart2@gmail.com>
2025-09-09 11:26:24 +0530

    Move GTM init to _app with next/script; keep noscript in _document
    - Implement Next.js-recommended GTM setup
    - Remove GTM loader from _document Head
    - Add GTM inline loader via next/script in _app (afterInteractive)
    - Keep noscript iframe in _document body
    - Avoid duplicate/competing GTM initializations
    
    This approach improves tag detection reliability and avoids Head script warnings.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -12,9 +13,24 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
+      {/* Google Tag Manager (init in _app with Next.js Script) */}
+      <Script id="gtm-init" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NV86HSCC');
+        `,
+      }} />
+
       <TooltipProvider>
         <Toaster />
         <Sonner />
+        {/* Listens to router changes and pushes pageview events */}
         <GTMListener />
         <Component {...pageProps} />
       </TooltipProvider>
